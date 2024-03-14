4a78ee7a642ed033fc48285b493b1108f53f0dd3
mateusgts007 <mateusgtsandrade@gmail.com>
2024-03-13 21:02:35 -0300

    Teste 1

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+       
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+
+        while (k < indice){
+
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        System.out.println(" o valor de soma: " + soma);
+
+    }
+}
